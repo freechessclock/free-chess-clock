42bbf450833385c32b7c7a91f1b818358984f277
Callum May <callumdmay@gmail.com>
2023-02-20 15:41:21 -0800

    fix opacity

--- src/settings.tsx
+++ src/settings.tsx
@@ -28,7 +28,7 @@ export default function Settings(props: SettingsProps) {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black bg-opacity-75 transition-opacity" />
+          <div className="fixed inset-0 bg-black bg-opacity-90 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto text-neutral-100">
