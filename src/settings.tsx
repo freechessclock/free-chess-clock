9f342ecbe5c358afef739dcbc6e805d16cece430
Callum May <callumdmay@gmail.com>
2023-02-20 18:57:36 -0800

    fix font

--- src/settings.tsx
+++ src/settings.tsx
@@ -172,7 +172,7 @@ export default function Settings(props: SettingsProps) {
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-neutral-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-neutral-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
+                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-neutral-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-neutral-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     onClick={() => props.setOpen(false)}
                   >
                     Done
