d830bad3391bfadfa798b18485a71f834e826751
Callum May <callumdmay@gmail.com>
2023-02-20 18:26:20 -0800

    fix font size

--- src/app.tsx
+++ src/app.tsx
@@ -90,9 +90,10 @@ export default function App() {
 
   const renderer = ({ hours, formatted }: CountdownRenderProps) => {
     return (
-      <div className={classNames('w-full h-full grow flex items-center justify-center text-8xl lg:text-9xl', { "text-7xl lg:text-8xl": hours })}>
-        {hours !== 0 ? `${formatted.hours}:` : <></>}{formatted.minutes}:{formatted.seconds}
-      </div>
+      <div className={classNames('w-full h-full grow flex items-center justify-center', { "text-7xl lg:text-8xl": hours }, { "text-8xl lg:text-9xl": !hours })}>
+        {hours !== 0 ? `${formatted.hours}:` : <></>
+        } {formatted.minutes}: {formatted.seconds}
+      </div >
     );
   };
 
